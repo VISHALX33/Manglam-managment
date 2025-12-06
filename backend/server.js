16326e8752fe42c165634c12d5981d875d92c55d
VISHALX33 <vishalprajapati2303@gmail.com>
2025-12-06 16:48:05 +0530

    link

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.FRONTEND_URL 
-    : ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    : ['http://localhost:3000',
+         'http://127.0.0.1:3000',
+         'https://manglammess.netlify.app',],
   credentials: true,
   optionsSuccessStatus: 200
 };
